bfe89c5462ce321e9e45a4070cac27b7d3233d0c
BielDias2501 <gabrieldiascl@gmail.com>
2025-03-12 14:12:43 -0300

    add: adicionei informações no vs code

---
+++ tarefa-01/src/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./pages/Home/index"
+import Contato from "./pages/Contato/index"
+import Sobre from "./pages/Sobre/index"
+
+function App(){
+    return(
+        <>
+        <Home />
+        </>
+    )
+}
+
+export default App
